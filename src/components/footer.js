7314417500fa583e10c08ec9c7324eeb3b99e28c
RoyFBaltazar <94885352+RoyFBaltazar@users.noreply.github.com>
2022-03-03 11:50:30 -0500

    Added footer2

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,17 +1,15 @@
 const Footer =()=>{
-    return(<div className="main-footer">
-        <div className="container">
-            <div className="row">
-
-            </div>
-            {/* Footer bottom */}
-            <div className="footer-bottom">
-                <p className="text-xs-center">
-                    &copy;{new Date().getFullYear()}
-                </p>
-            </div>
-
+    return(<div className="container">
+        <div className="wrapper">
+        
+        <div className="row">
+        <div className="column"><a href="https://pokeapi.co/">Poke API</a></div>
+        <div className="column"><a href="https://github.com/RoyFBaltazar/favPokemon">GitHub</a></div>
+        <div className="column"><a href="https://www.linkedin.com/in/roy-baltazar-9b42a5228/">Linkedin</a></div>
         </div>
-    </div>)
+        
+    </div>
+    </div>
+    )
 }
 export default Footer;
